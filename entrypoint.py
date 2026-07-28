e09e9629c2fc1766c8a1e3b646a9b4d1e9087895
Tamanna Akter <tamannaakter.cse23@gmail.com>
2026-07-29 02:47:22 +0600

    Update entrypoint with Tamanna superuser

--- entrypoint.py
+++ entrypoint.py
@@ -1,13 +1,15 @@
+
 #!/bin/sh
 
-# ডাটাবেজ মাইগ্রেশন রান করা
+# ১. প্রথমে ডাটাবেজ মাইগ্রেশন রান করা বাধ্যতামূলক
+python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
-# স্ট্যাটিক ফাইল কালেক্ট করা
+# ২. স্ট্যাটিক ফাইল কালেক্ট করা
 python manage.py collectstatic --noinput
 
-# সুপারইউজার তৈরি (Username: Tamanna এবং Password: admin123)
+# ৩. সুপারইউজার তৈরি করা
 python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='Tamanna').exists() or User.objects.create_superuser('Tamanna', 'tamanna@example.com', 'admin123')"
 
-# গিউনিকর্ন সার্ভার স্টার্ট করা
+# ৪. সার্ভার স্টার্ট করা
 exec gunicorn config.wsgi:application --bind 0.0.0.0:10000
